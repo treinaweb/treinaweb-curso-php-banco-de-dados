4820180ef48f46e30fa2b22395d895799ad4e4cb
Elton Fonseca <elton869@gmail.com>
2020-07-16 17:05:29 -0300

    17 - Tratando erros de executação SQL

--- index.php
+++ index.php
@@ -4,12 +4,19 @@ try {
     $conexao = new PDO('mysql:host=127.0.0.1;dbname=treinaweb', 'root', '123456789');
 
     $resultado = $conexao->query("select * from alunos");
+
+    if (!$resultado) {
+        $erro = $conexao->errorInfo();
+
+        throw new \PDOException($erro[2], $erro[1]); 
+    }
     
     $alunos = $resultado->fetchAll(PDO::FETCH_ASSOC);
     var_dump($alunos);
 
 } catch (\PDOException $e) {
-    echo $e->getMessage();
+    echo "mensagem: " . $e->getMessage();
+    echo "<br>Codigo: " . $e->getCode();
 }
 
 
