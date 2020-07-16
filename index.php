b150beee5b34ade33ff7647ed97877e6529d7fa1
Elton Fonseca <elton869@gmail.com>
2020-07-16 16:52:22 -0300

    15 - Executando consultas com método Query

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
 try {
-    $conexao = new PDO('mysql:host=5555127.0.0.1;dbname:treinaweb', 'root', '123456789');
+    $conexao = new PDO('mysql:host=127.0.0.1;dbname=treinaweb', 'root', '123456789');
+
+    $resultado = $conexao->query("select * from alunos");
     
-    var_dump($conexao);
+    foreach ($resultado as $aluno) {
+        var_dump($aluno);
+    }
 
 } catch (\PDOException $e) {
     echo $e->getMessage();
