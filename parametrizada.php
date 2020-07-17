38b94c7080bce50ae319b4add2709b84e2c2484e
Elton Fonseca <elton869@gmail.com>
2020-07-17 11:43:22 -0300

    19 - Parâmetros nomeados e tipos de bind

--- parametrizada.php
+++ parametrizada.php
@@ -2,11 +2,29 @@
 
 $conexao = new PDO('mysql:host=127.0.0.1;dbname=treinaweb', 'root', '123456789');
 
+//Parâmetro nomeado e bind pela referência 
+$termoBusca = '%maria%';
+
+$query = $conexao->prepare('SELECT * FROM alunos WHERE nome like :termobusca');
+
+$query->bindParam(':termobusca', $termoBusca, PDO::PARAM_STR);
+
+$termoBusca = '%Silva%';
+
+$query->execute();
+
+foreach ($query as $linha) {
+    var_dump($linha);
+}
+
+//Parâmetro indexado e bind pelo valor diretamente
 $id = 2;
 
 $query = $conexao->prepare('SELECT * FROM alunos where id = ?');
 
 $query->bindValue(1, $id, PDO::PARAM_INT);
+
+$id = 3;
 
 $query->execute();
 
