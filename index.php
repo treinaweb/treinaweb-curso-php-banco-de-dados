15a1509386b25999b8a0bc6b895df09b0a446dac
Elton Fonseca <elton869@gmail.com>
2020-07-17 12:09:05 -0300

    20 - Modos de tratamento de erro do PDO

--- index.php
+++ index.php
@@ -3,13 +3,9 @@
 try {
     $conexao = new PDO('mysql:host=127.0.0.1;dbname=treinaweb', 'root', '123456789');
 
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     $resultado = $conexao->query("select * from alunos");
-
-    if (!$resultado) {
-        $erro = $conexao->errorInfo();
-
-        throw new \PDOException($erro[2], $erro[1]); 
-    }
     
     $alunos = $resultado->fetchAll(PDO::FETCH_ASSOC);
     var_dump($alunos);
